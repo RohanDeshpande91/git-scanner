a915c8c827c4a21cec1e1b8c6d7515c13b5eaf58
RohanDeshpande91 <rohandeshpande91@gmail.com>
2023-10-09 13:16:42 +0530

    resolver

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -2,15 +2,6 @@ import { getRepos } from "./service";
 type repoDetails = {
     repoName: string
 }
-async function gtrep (){
-  const data = await getRepos();
-  console.log(data);
-}
-//gtrep();
-/*const resolvers = {
-    listRepo: async () => await getRepos(),
-    repoDetails: async (_: any, { repoName }: repoDetails) => await getRepos(repoName)
-}*/
 
 const resolvers = {
   Query: {
